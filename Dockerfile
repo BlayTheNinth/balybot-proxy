4e32033a5e9bff8ac052eb449704f0bc8dafb800
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-09-02 08:10:21 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:20-alpine
+WORKDIR /home/node/app
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+COPY --chown=node:node package*.json ./
+USER node
+RUN npm install
+COPY --chown=node:node . .
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const hooksFilePath = path.join(__dirname, 'hooks.json');
+
+try {
+    const hooksData = fs.readFileSync(hooksFilePath, 'utf8');
+    hooks = JSON.parse(hooksData);
+    for (const hook of hooks) {
+        if (hook.pattern) {
+            hook.pattern = new RegExp(hook.pattern, 'i');
+        }
+    }
+} catch (error) {
+    console.error('Error loading hooks:', error);
+}
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.MessageCreate, async (msg) => {
+    for (const hook of hooks) {
+        const match = msg.content.match(hook.pattern);
+        if (match) {
+            msg.channel.sendTyping();
+            const response = await fetch(hook.webhook, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    content: match[1],
+                }),
+            });
+            const json = await response.json();
+            if (json.interactionResponse) {
+                msg.reply(json.interactionResponse);
+            } else {
+                console.log('Did not get an interaction response from webhook: ', json);
+            }
+        }
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
